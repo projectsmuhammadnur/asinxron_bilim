eb4fa4813b8b77a747f913be9a5a7719c41ba6cc
muhammadnur <muhammadnurpdp@gmail.com>
2023-12-25 16:22:31 +0500

    add allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-wskcuw#-*1)%q@vhagvluca%11&++++=il)h+m_j^&m%c4zp'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["90.156.229.62", "127.0.0.1"]
+ALLOWED_HOSTS = ["91.186.199.96", "127.0.0.1"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
